bff734e589d2082ecf77aa90beb09a69ea6b5288
maji <ji.ma@eeoa.com>
2020-10-31 11:51:36 +0800

    发布V1.1.0版本

---
+++ testProject/testRep/MJLog.h
@@ -0,0 +1,17 @@
+//
+//  MJLog.h
+//  testProject
+//
+//  Created by maji on 2020/10/31.
+//  Copyright © 2020 maji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJLog : NSObject
+- (NSString *)mjCurrentVersion;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testProject/testRep/MJLog.m
@@ -0,0 +1,15 @@
+//
+//  MJLog.m
+//  testProject
+//
+//  Created by maji on 2020/10/31.
+//  Copyright © 2020 maji. All rights reserved.
+//
+
+#import "MJLog.h"
+
+@implementation MJLog
+- (NSString *)mjCurrentVersion {
+    return @"V 1.1.0 版本";
+}
+@end
